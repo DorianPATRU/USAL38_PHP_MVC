ee23bcfc1f88c1a80a9d6a03f87268711e3879a5
DPatru <73827969+DorianPATRU@users.noreply.github.com>
2020-12-13 22:24:00 +0100

    update

--- VSC - PHP_MVC/php_intro_exercices/02-numbers.php
+++ VSC - PHP_MVC/php_intro_exercices/02-numbers.php
@@ -35,5 +35,51 @@ echo getSub2($nb3, $nb4);
 
 */
 
+/* 2.C
 
+$nb1 = 5.6;
+$nb2 = 3;
 
+function getMulti1($nb1, $nb2) {
+    return $nb1 * $nb2;
+} // retourne 16.8
+
+echo getMulti1($nb1, $nb2);
+
+$nb3 = 5.6;
+$nb4 = -3.7;
+
+function getMulti2($nb3, $nb4) {
+    return $nb3 * $nb4;
+} // retourne -20.72
+
+echo getMulti2($nb3, $nb4);
+
+*/
+
+/* 2.D
+
+$nb1 = 20;
+$nb2 = 3;
+
+function getDiv1($nb1, $nb2) {
+    return (round($nb1 / $nb2, 2));
+} // retourne 8.33 (erreur) --> 6.67
+
+echo getDiv1($nb1, $nb2);
+
+$nb3 = 20;
+$nb4 = 0;
+
+function getDiv2($nb3, $nb4) {
+    if ($nb3 / $nb4 = 0) {
+        return 0;
+    }
+} // retourne 0
+
+echo getDiv2($nb3, $nb4);
+
+*/
+
+// 0 s'affiche dans la console mais il y a le message "warning division par 0" en plus, 
+// je n'ai pas trouver la solution pour le moment
